74cb74491cc9d75554a5de9bad08ceb0b3ace8bf
sonyou22 <sonyou22@gmail.com>
2015-03-03 17:41:26 +0900

    css, img, js update

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function() {
+    'use strict';
+
+    $('.kumc').hover(function() {
+       $('.bg').css('background-image', 'url("css/img/BG/1_KUMC.jpg")');
+    });
+
+    $('.uncomb').hover(function() {
+        $('.bg').css('background-image', 'url("css/img/BG/2_uncomb.jpg")');
+    });
+
+    $('.d3').hover(function() {
+        $('.bg').css('background-image', 'url("css/img/BG/3_D3.jpg")');
+    });
+
+    $('.ice').hover(function() {
+        $('.bg').css('background-image', 'url("css/img/BG/4_ICE.jpg")');
+    });
+
+    $('.nobody').hover(function() {
+        $('.bg').css('background-image', 'url("css/img/BG/5_Nobody.jpg")');
+    });
+}());
