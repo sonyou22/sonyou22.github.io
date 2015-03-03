2d707ff4276db4a847d909dc492c8a197eaee17c
sonyou22 <sonyou22@gmail.com>
2015-03-03 18:39:57 +0900

    150303-3

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@
     });
 
     $('.d3').hover(function() {
-        $('.bg').css('background-image', 'url("css/img/BG/3_D3.jpg")');
+        $('.bg').css('background-image', 'url("css/img/BG/3_d3jubilee.jpg")');
     });
 
     $('.ice').hover(function() {
